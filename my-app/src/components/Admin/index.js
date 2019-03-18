731ea90b5c224d8ccb4c32f8dffecfdf93cda6a7
Amy Kim <yk154@duke.edu>
2019-03-18 16:53:43 -0400

    routes set up

---
+++ my-app/src/components/Admin/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+class Admin extends React.Component {
+  render() {
+    return (
+      <div className="main">
+      <div className="header">
+        <p>Admin Page</p>
+        <hr></hr>
+      </div>
+      </div>
+    )
+  }
+}
+export default Admin
--- my-app/src/components/App/index.js
+++ my-app/src/components/App/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+BrowserRouter as Router,
+Route,
+} from 'react-router-dom';
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import HomePage from '../Home';
+import AdminPage from '../Admin';
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
 );
-
 export default App;
